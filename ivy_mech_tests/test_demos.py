1d087cf16dabe7df9f8dfd406f9b745a9633539e
djl11 <daniel.lenton11@imperial.ac.uk>
2021-08-01 00:10:37 +0200

    updated Dockerfile to include CoppeliaSim and PyRep, but not yet got PyRep running without segfaulting, next to fix this issue.

--- ivy_mech_tests/test_demos.py
+++ ivy_mech_tests/test_demos.py
@@ -15,17 +15,21 @@ def test_demo_run_through(dev_str, f, call):
     main(f=f)
 
 
-def test_demo_target_facing_rotation_vector(dev_str, f, call):
+@pytest.mark.parametrize(
+    "with_sim", [False])
+def test_demo_target_facing_rotation_vector(with_sim, dev_str, f, call):
     from demos.interactive.target_facing_rotation_matrix import main
     if call in [helpers.tf_graph_call]:
         # these particular demos are only implemented in eager mode, without compilation
         pytest.skip()
-    main(False, False, f=f)
+    main(False, with_sim, f=f)
 
 
-def test_demo_polar_to_cartesian_coords(dev_str, f, call):
+@pytest.mark.parametrize(
+    "with_sim", [False])
+def test_demo_polar_to_cartesian_coords(with_sim, dev_str, f, call):
     from demos.interactive.polar_to_cartesian_coords import main
     if call in [helpers.tf_graph_call]:
         # these particular demos are only implemented in eager mode, without compilation
         pytest.skip()
-    main(False, False, f=f)
+    main(False, with_sim, f=f)
