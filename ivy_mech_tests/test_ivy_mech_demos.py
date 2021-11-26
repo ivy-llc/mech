c24fab87152cf480792ecd59576ed80fd2f27035
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-26 16:42:43 +0100

    renamed demos folder to ivy_mech_demos.

--- ivy_mech_tests/test_ivy_mech_demos.py
+++ ivy_mech_tests/test_ivy_mech_demos.py
@@ -8,7 +8,7 @@ import ivy_tests.helpers as helpers
 
 
 def test_demo_run_through(dev_str, f, call):
-    from demos.run_through import main
+    from ivy_mech_demos.run_through import main
     if call in [helpers.tf_graph_call]:
         # these particular demos are only implemented in eager mode, without compilation
         pytest.skip()
@@ -18,7 +18,7 @@ def test_demo_run_through(dev_str, f, call):
 @pytest.mark.parametrize(
     "with_sim", [False])
 def test_demo_target_facing_rotation_vector(with_sim, dev_str, f, call):
-    from demos.interactive.target_facing_rotation_matrix import main
+    from ivy_mech_demos.interactive.target_facing_rotation_matrix import main
     if call in [helpers.tf_graph_call]:
         # these particular demos are only implemented in eager mode, without compilation
         pytest.skip()
@@ -28,7 +28,7 @@ def test_demo_target_facing_rotation_vector(with_sim, dev_str, f, call):
 @pytest.mark.parametrize(
     "with_sim", [False])
 def test_demo_polar_to_cartesian_coords(with_sim, dev_str, f, call):
-    from demos.interactive.polar_to_cartesian_coords import main
+    from ivy_mech_demos.interactive.polar_to_cartesian_coords import main
     if call in [helpers.tf_graph_call]:
         # these particular demos are only implemented in eager mode, without compilation
         pytest.skip()
