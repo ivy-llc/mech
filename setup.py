bb3df4149dae2a9c0041918caae22df2a1cf2520
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-28 13:38:02 +0100

    version 1.1.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='ivy-mech',
-      version='1.1.5',
+      version='1.1.6',
       author='Ivy Team',
       author_email='ivydl.team@gmail.com',
       description='Mechanics functions with end-to-end support for deep learning developers, written in Ivy',
