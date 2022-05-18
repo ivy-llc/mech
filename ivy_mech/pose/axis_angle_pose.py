1cefaed5a0aabbb2b3a43e3e0791643a300f5bf1
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-05-18 11:37:43 +0100

    ivy_mech/pose docstring update

--- ivy_mech/pose/axis_angle_pose.py
+++ ivy_mech/pose/axis_angle_pose.py
@@ -1,6 +1,4 @@
-"""
-Collection of Pose Conversion Functions to Axis-Angle Pose Format
-"""
+"""Collection of Pose Conversion Functions to Axis-Angle Pose Format"""
 
 # global
 import ivy as _ivy
@@ -11,20 +9,26 @@ from ivy_mech.orientation import axis_angle as _ivy_aa
 
 
 def euler_pose_to_axis_angle_pose(euler_pose, convention='zyx', batch_shape=None, dev_str=None):
-    """
-    Convert :math: Euler angle pose
+    """Convert :math: Euler angle pose
     :math:`\mathbf{p}_{abc} = [\mathbf{x}_c, \mathbf{θ}_{xyz}] = [x, y, z, ϕ_a, ϕ_b, ϕ_c]` to
     axis-angle pose :math:`\mathbf{p}_{aa} = [\mathbf{x}_c, \mathbf{e}, θ] = [x, y, z, e_x, e_y, e_z, θ]`
 
-    :param euler_pose: Euler angle pose *[batch_shape,6]*
+    Parameters
+    ----------
+    euler_pose
+        Euler angle pose *[batch_shape,6]*
+    convention
+        The axes for euler rotation, in order of L.H.S. matrix multiplication. (Default value = 'zyx')
+    batch_shape
+        Shape of batch. Inferred from inputs if None. (Default value = None)
+    dev_str
+        device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc. Same as x if None. (Default value = None)
 
-    :param convention: The axes for euler rotation, in order of L.H.S. matrix multiplication.
+    Returns
+    -------
+    ret
+        Rotation axis unit vector and angle *[batch_shape,4]*
 
-    :param batch_shape: Shape of batch. Inferred from inputs if None.
-
-    :param dev_str: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc. Same as x if None.
-
-    :return: Rotation axis unit vector and angle *[batch_shape,4]*
     """
     aa = _ivy_aa.euler_to_axis_angle(euler_pose[..., 3:], convention, batch_shape, dev_str)
     return _ivy.concatenate([euler_pose[..., :3], aa], -1)
@@ -32,14 +36,20 @@ def euler_pose_to_axis_angle_pose(euler_pose, convention='zyx', batch_shape=None
 
 # noinspection PyUnresolvedReferences
 def mat_pose_to_rot_vec_pose(matrix):
-    """
-    Convert matrix pose :math:`\mathbf{P}\in\mathbb{R}^{3×4}` to rotation vector pose
+    """Convert matrix pose :math:`\mathbf{P}\in\mathbb{R}^{3×4}` to rotation vector pose
     :math:`\mathbf{p}_{rv} = [\mathbf{x}_c, \mathbf{θ}_{rv}] = [x, y, z, θe_x, θe_y, θe_z]`.\n
     `[reference] <https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions#Euler_axis_and_angle_(rotation_vector)>`_
 
-    :param matrix: Matrix pose *[batch_shape,3,4]*
+    Parameters
+    ----------
+    matrix
+        Matrix pose *[batch_shape,3,4]*
 
-    :return: Rotation vector pose *[batch_shape,6]*
+    Returns
+    -------
+    ret
+        Rotation vector pose *[batch_shape,6]*
+
     """
 
     # BS x 3
@@ -57,14 +67,20 @@ def mat_pose_to_rot_vec_pose(matrix):
 
 # noinspection PyUnresolvedReferences
 def quaternion_pose_to_rot_vec_pose(quat_pose):
-    """
-    Convert quaternion pose :math:`\mathbf{p}_{q} = [\mathbf{x}_c, \mathbf{q}] = [x, y, z, q_i, q_j, q_k, q_r]` to
+    """Convert quaternion pose :math:`\mathbf{p}_{q} = [\mathbf{x}_c, \mathbf{q}] = [x, y, z, q_i, q_j, q_k, q_r]` to
     rotation vector pose :math:`\mathbf{p}_{rv} = [\mathbf{x}_c, \mathbf{θ}_{rv}] = [x, y, z, θe_x, θe_y, θe_z]`.\n
     `[reference] <https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions#Euler_axis_and_angle_(rotation_vector)>`_
 
-    :param quat_pose: Quaternion pose *[batch_shape,7]*
+    Parameters
+    ----------
+    quat_pose
+        Quaternion pose *[batch_shape,7]*
 
-    :return: Rotation vector pose *[batch_shape,6]*
+    Returns
+    -------
+    ret
+        Rotation vector pose *[batch_shape,6]*
+
     """
 
     # BS x 4
