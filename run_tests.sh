bf9e66ef6d4a27a98ae294895104840093156653
pawasRituja <ritujapawas.djr@gmail.com>
2022-06-22 23:51:19 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/mech unifyai/ivy-mech:latest python3 -m pytest ivy/ivy_mech_tests/
+docker run --rm -it -v "$(pwd)":/mech unifyai/mech:latest python3 -m pytest ivy/ivy_mech_tests/
