7f0f3550b8ddab66c6ce38744cd17a42d74bfcc0
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-31 06:41:37 +0000

    fix quarternions tests

--- ivy_mech_tests/test_orientation/test_quaternions.py
+++ ivy_mech_tests/test_orientation/test_quaternions.py
@@ -19,22 +19,22 @@ qtd = QuaternionTestData()
 
 def test_vector_and_angle_to_quaternion(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.axis_angle_to_quaternion(qtd.axis_angle), qtd.quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.axis_angle_to_quaternion(qtd.batched_axis_angle)[0], qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.axis_angle_to_quaternion(ivy.array(qtd.axis_angle)), qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.axis_angle_to_quaternion(ivy.array(qtd.batched_axis_angle))[0], qtd.quaternion, atol=1e-6)
     ivy.unset_backend()
     
 
 def test_axis_angle_to_quaternion(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.polar_axis_angle_to_quaternion(qtd.polar_axis_angle), qtd.quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.polar_axis_angle_to_quaternion(qtd.batched_polar_axis_angle)[0], qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.polar_axis_angle_to_quaternion(ivy.array(qtd.polar_axis_angle)), qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.polar_axis_angle_to_quaternion(ivy.array(qtd.batched_polar_axis_angle))[0], qtd.quaternion, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_rot_mat_to_quaternion(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.rot_mat_to_quaternion(qtd.rotation_matrix), qtd.quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.rot_mat_to_quaternion(qtd.batched_rotation_matrix)[0], qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.rot_mat_to_quaternion(ivy.array(qtd.rotation_matrix)), qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.rot_mat_to_quaternion(ivy.array(qtd.batched_rotation_matrix))[0], qtd.quaternion, atol=1e-6)
     ivy.unset_backend()
 
 
@@ -43,16 +43,16 @@ def test_euler_to_quaternion(device, fw):
         with ivy_np.use:
             quaternion = ivy_mech.euler_to_quaternion(qtd.euler_angles, conv)
         ivy.set_backend(fw)
-        assert np.allclose(ivy_mech.euler_to_quaternion(qtd.euler_angles, conv), quaternion, atol=1e-6)
-        assert np.allclose(ivy_mech.euler_to_quaternion(qtd.batched_euler_angles, conv)[0], quaternion,
+        assert np.allclose(ivy_mech.euler_to_quaternion(ivy.array(qtd.euler_angles), conv), quaternion, atol=1e-6)
+        assert np.allclose(ivy_mech.euler_to_quaternion(ivy.array(qtd.batched_euler_angles), conv)[0], quaternion,
                            atol=1e-6)
         ivy.unset_backend()
 
 
 def test_rotation_vector_to_quaternion(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.rotation_vector_to_quaternion(qtd.rotation_vector), qtd.quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.rotation_vector_to_quaternion(qtd.batched_rotation_vector)[0],
+    assert np.allclose(ivy_mech.rotation_vector_to_quaternion(ivy.array(qtd.rotation_vector)), qtd.quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.rotation_vector_to_quaternion(ivy.array(qtd.batched_rotation_vector))[0],
                        qtd.quaternion, atol=1e-6)
     ivy.unset_backend()
 
@@ -63,7 +63,7 @@ def test_rotation_vector_to_quaternion(device, fw):
 def test_inverse_quaternion(device, fw):
     ivy.set_backend(fw)
     assert np.allclose(ivy_mech.inverse_quaternion(
-                            ivy_mech.inverse_quaternion(qtd.quaternion)),
+                            ivy_mech.inverse_quaternion(ivy.array(qtd.quaternion))),
                        qtd.quaternion, atol=1e-6)
     ivy.unset_backend()
 
@@ -77,16 +77,16 @@ def test_get_random_quaternion(device, fw):
 
 def test_scale_quaternion_theta(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.scale_quaternion_rotation_angle(qtd.quaternion, qtd.scale_factor), qtd.scaled_quaternion,
+    assert np.allclose(ivy_mech.scale_quaternion_rotation_angle(ivy.array(qtd.quaternion), ivy.array(qtd.scale_factor)), qtd.scaled_quaternion,
                        atol=1e-6)
-    assert np.allclose(ivy_mech.scale_quaternion_rotation_angle(qtd.batched_quaternion, qtd.batched_scale_factor)[0],
+    assert np.allclose(ivy_mech.scale_quaternion_rotation_angle(ivy.array(qtd.batched_quaternion), ivy.array(qtd.batched_scale_factor))[0],
                        qtd.scaled_quaternion, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_hamilton_product(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.hamilton_product(qtd.quaternion, qtd.quaternion), qtd.hp_quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.hamilton_product(qtd.batched_quaternion, qtd.batched_quaternion)[0],
+    assert np.allclose(ivy_mech.hamilton_product(ivy.array(qtd.quaternion), ivy.array(qtd.quaternion)), qtd.hp_quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.hamilton_product(ivy.array(qtd.batched_quaternion), ivy.array(qtd.batched_quaternion))[0],
                        qtd.hp_quaternion, atol=1e-6)
     ivy.unset_backend()
