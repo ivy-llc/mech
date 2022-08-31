bebe9da40e5e4c5860ea783eb2e1b9a145bceb76
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-31 06:52:38 +0000

    fix quaternion_poses tests

--- ivy_mech_tests/test_pose/test_quaternion_poses.py
+++ ivy_mech_tests/test_pose/test_quaternion_poses.py
@@ -15,32 +15,32 @@ ptd = PoseTestData()
 
 def test_axis_angle_pose_to_quaternion_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.axis_angle_pose_to_quaternion_pose(ptd.axis_angle_pose), ptd.quaternion_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.axis_angle_pose_to_quaternion_pose(ptd.batched_axis_angle_pose)[0],
+    assert np.allclose(ivy_mech.axis_angle_pose_to_quaternion_pose(ivy.array(ptd.axis_angle_pose)), ptd.quaternion_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.axis_angle_pose_to_quaternion_pose(ivy.array(ptd.batched_axis_angle_pose))[0],
                        ptd.quaternion_pose, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_mat_pose_to_quaternion_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.mat_pose_to_quaternion_pose(ptd.matrix_pose), ptd.quaternion_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.mat_pose_to_quaternion_pose(ptd.batched_matrix_pose)[0],
+    assert np.allclose(ivy_mech.mat_pose_to_quaternion_pose(ivy.array(ptd.matrix_pose)), ptd.quaternion_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.mat_pose_to_quaternion_pose(ivy.array(ptd.batched_matrix_pose))[0],
                        ptd.quaternion_pose, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_euler_pose_to_quaternion_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.euler_pose_to_quaternion_pose(ptd.euler_pose), ptd.quaternion_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.euler_pose_to_quaternion_pose(ptd.batched_euler_pose)[0],
+    assert np.allclose(ivy_mech.euler_pose_to_quaternion_pose(ivy.array(ptd.euler_pose)), ptd.quaternion_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.euler_pose_to_quaternion_pose(ivy.array(ptd.batched_euler_pose))[0],
                        ptd.quaternion_pose, atol=1e-6)
     ivy.unset_backend() 
 
 
 def test_increment_quaternion_pose_with_velocity(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.increment_quaternion_pose_with_velocity(ptd.quaternion_pose, ptd.velocity,
-                            ptd.control_dt), ptd.incremented_quaternion, atol=1e-6)
-    assert np.allclose(ivy_mech.increment_quaternion_pose_with_velocity(ptd.batched_quaternion_pose,
-                            ptd.batched_velocity, ptd.batched_control_dt)[0], ptd.incremented_quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.increment_quaternion_pose_with_velocity(ivy.array(ptd.quaternion_pose), ivy.array(ptd.velocity),
+                            ivy.array(ptd.control_dt)), ptd.incremented_quaternion, atol=1e-6)
+    assert np.allclose(ivy_mech.increment_quaternion_pose_with_velocity(ivy.array(ptd.batched_quaternion_pose),
+                            ivy.array(ptd.batched_velocity), ivy.array(ptd.batched_control_dt))[0], ptd.incremented_quaternion, atol=1e-6)
     ivy.unset_backend()
