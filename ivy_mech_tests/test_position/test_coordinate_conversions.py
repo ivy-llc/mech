86e52463e7b7cdbbda02d4f09efbe9c056bb0a6d
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-31 06:55:15 +0000

    fix position related tests

--- ivy_mech_tests/test_position/test_coordinate_conversions.py
+++ ivy_mech_tests/test_position/test_coordinate_conversions.py
@@ -15,16 +15,16 @@ ptd = PositionTestData()
 
 def test_polar_to_cartesian_coords(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.polar_to_cartesian_coords(ptd.polar_coords), ptd.cartesian_coords, atol=1e-6)
-    assert np.allclose(ivy_mech.polar_to_cartesian_coords(ptd.batched_polar_coords)[0],
+    assert np.allclose(ivy_mech.polar_to_cartesian_coords(ivy.array(ptd.polar_coords)), ptd.cartesian_coords, atol=1e-6)
+    assert np.allclose(ivy_mech.polar_to_cartesian_coords(ivy.array(ptd.batched_polar_coords))[0],
                        ptd.cartesian_coords, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_cartesian_to_polar_coords(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.cartesian_to_polar_coords(ptd.cartesian_coords), ptd.polar_coords, atol=1e-6)
-    assert np.allclose(ivy_mech.cartesian_to_polar_coords(ptd.batched_cartesian_coords)[0],
+    assert np.allclose(ivy_mech.cartesian_to_polar_coords(ivy.array(ptd.cartesian_coords)), ptd.polar_coords, atol=1e-6)
+    assert np.allclose(ivy_mech.cartesian_to_polar_coords(ivy.array(ptd.batched_cartesian_coords))[0],
                        ptd.polar_coords, atol=1e-6)
     ivy.unset_backend()
 
@@ -32,10 +32,10 @@ def test_cartesian_to_polar_coords(device, fw):
 def test_cartesian_to_polar_coords_and_back(device, fw):
     ivy.set_backend(fw)
     assert np.allclose(ivy_mech.polar_to_cartesian_coords(
-                            ivy_mech.cartesian_to_polar_coords(ptd.cartesian_coords)),
+                            ivy_mech.cartesian_to_polar_coords(ivy.array(ptd.cartesian_coords))),
                        ptd.cartesian_coords, atol=1e-6)
     assert np.allclose(ivy_mech.polar_to_cartesian_coords(
-                            ivy_mech.cartesian_to_polar_coords(ptd.batched_cartesian_coords))[0],
+                            ivy_mech.cartesian_to_polar_coords(ivy.array(ptd.batched_cartesian_coords)))[0],
                        ptd.cartesian_coords, atol=1e-6)
     ivy.unset_backend()
 
@@ -43,9 +43,9 @@ def test_cartesian_to_polar_coords_and_back(device, fw):
 def test_polar_to_cartesian_coords_and_back(device, fw):
     ivy.set_backend(fw)
     assert np.allclose(ivy_mech.cartesian_to_polar_coords(
-                            ivy_mech.polar_to_cartesian_coords(ptd.polar_coords)),
+                            ivy_mech.polar_to_cartesian_coords(ivy.array(ptd.polar_coords))),
                        ptd.polar_coords, atol=1e-6)
     assert np.allclose(ivy_mech.cartesian_to_polar_coords(
-                            ivy_mech.polar_to_cartesian_coords(ptd.batched_polar_coords))[0],
+                            ivy_mech.polar_to_cartesian_coords(ivy.array(ptd.batched_polar_coords)))[0],
                        ptd.polar_coords, atol=1e-6)
     ivy.unset_backend()
--- ivy_mech_tests/test_position/test_transformations.py
+++ ivy_mech_tests/test_position/test_transformations.py
@@ -15,16 +15,16 @@ ptd = PositionTestData()
 
 def test_make_coordinates_homogeneous(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.make_coordinates_homogeneous(ptd.cartesian_coords),
+    assert np.allclose(ivy_mech.make_coordinates_homogeneous(ivy.array(ptd.cartesian_coords)),
                        ptd.cartesian_coords_homo, atol=1e-6)
-    assert np.allclose(ivy_mech.make_coordinates_homogeneous(ptd.batched_cartesian_coords),
+    assert np.allclose(ivy_mech.make_coordinates_homogeneous(ivy.array(ptd.batched_cartesian_coords)),
                        ptd.batched_cartesian_coords_homo, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_make_transformation_homogeneous(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.make_transformation_homogeneous(ptd.ext_mat), ptd.ext_mat_homo, atol=1e-6)
-    assert np.allclose(ivy_mech.make_transformation_homogeneous(ptd.batched_ext_mat),
+    assert np.allclose(ivy_mech.make_transformation_homogeneous(ivy.array(ptd.ext_mat)), ptd.ext_mat_homo, atol=1e-6)
+    assert np.allclose(ivy_mech.make_transformation_homogeneous(ivy.array(ptd.batched_ext_mat)),
                        ptd.batched_ext_mat_homo, atol=1e-6)
     ivy.unset_backend()
