dec3133541532c36cd38b3e21f206609d7ca86e1
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-31 06:52:23 +0000

    fix matrix_poses tests

--- ivy_mech_tests/test_pose/test_matrix_poses.py
+++ ivy_mech_tests/test_pose/test_matrix_poses.py
@@ -16,16 +16,16 @@ ptd = PoseTestData()
 
 def test_axis_angle_pose_to_mat_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.axis_angle_pose_to_mat_pose(ptd.axis_angle_pose), ptd.matrix_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.axis_angle_pose_to_mat_pose(ptd.batched_axis_angle_pose)[0],
+    assert np.allclose(ivy_mech.axis_angle_pose_to_mat_pose(ivy.array(ptd.axis_angle_pose)), ptd.matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.axis_angle_pose_to_mat_pose(ivy.array(ptd.batched_axis_angle_pose))[0],
                        ptd.matrix_pose, atol=1e-6)
     ivy.unset_backend()
     
 
 def test_quaternion_pose_to_mat_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.quaternion_pose_to_mat_pose(ptd.quaternion_pose), ptd.matrix_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.quaternion_pose_to_mat_pose(ptd.batched_quaternion_pose)[0],
+    assert np.allclose(ivy_mech.quaternion_pose_to_mat_pose(ivy.array(ptd.quaternion_pose)), ptd.matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.quaternion_pose_to_mat_pose(ivy.array(ptd.batched_quaternion_pose))[0],
                        ptd.matrix_pose, atol=1e-6)
     ivy.unset_backend()
 
@@ -34,13 +34,13 @@ def test_euler_pose_to_mat_pose(device, fw):
     with ivy_np.use:
         matrix_pose = ivy_mech.euler_pose_to_mat_pose(ptd.euler_pose)
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.euler_pose_to_mat_pose(ptd.euler_pose), matrix_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.euler_pose_to_mat_pose(ptd.batched_euler_pose)[0], matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.euler_pose_to_mat_pose(ivy.array(ptd.euler_pose)), matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.euler_pose_to_mat_pose(ivy.array(ptd.batched_euler_pose))[0], matrix_pose, atol=1e-6)
     ivy.unset_backend()
 
 
 def test_rot_vec_pose_to_mat_pose(device, fw):
     ivy.set_backend(fw)
-    assert np.allclose(ivy_mech.rot_vec_pose_to_mat_pose(ptd.rot_vec_pose), ptd.matrix_pose, atol=1e-6)
-    assert np.allclose(ivy_mech.rot_vec_pose_to_mat_pose(ptd.batched_rot_vec_pose)[0], ptd.matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.rot_vec_pose_to_mat_pose(ivy.array(ptd.rot_vec_pose)), ptd.matrix_pose, atol=1e-6)
+    assert np.allclose(ivy_mech.rot_vec_pose_to_mat_pose(ivy.array(ptd.batched_rot_vec_pose))[0], ptd.matrix_pose, atol=1e-6)
     ivy.unset_backend()
