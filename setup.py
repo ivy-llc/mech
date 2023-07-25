40af0bdbf18be29865e5a25f33b1443223be6a40
juliagsy <67888047+juliagsy@users.noreply.github.com>
2023-07-25 15:11:51 +0800

    update version

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ long_description = "\n".join(lines)
 
 setup(
     name="ivy-mech",
-    version="0.0.0",
+    version="0.0.0.dev0",
     author="Unify",
     author_email="hello@unify.ai",
     description=(
